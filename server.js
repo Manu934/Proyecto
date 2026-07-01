ed89f0302d090f52e93cade89964f3d13a4491c7
Manu934 <49194128@est.ort.edu.ar>
2026-07-01 15:32:40 -0300

    Update server.js

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use(cors({
 app.use(express.json());
 app.use(passport.initialize());
 app.use(express.static(join(__dirname, "public")));
+app.use("/uploads", express.static(join(__dirname, "uploads")));
 
 app.use("/api/pruebas", pruebasRoutes);
 app.use("/api/admin", adminRoutes);
